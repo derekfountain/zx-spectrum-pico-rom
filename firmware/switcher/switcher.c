42763fe78d5812542437093b4fb9e5b0d12b4433
Derek Fountain <derek@scratters.com>
2023-01-31 17:06:43 +0000

    border effect rom in the main code, works on hardware

--- firmware/switcher/switcher.c
+++ firmware/switcher/switcher.c
@@ -11,10 +11,16 @@
 
 /* Don't use statics, the RAM model isn't set up for that */
 
+void print_str( uint8_t x, uint8_t y, uint8_t *str );
+void print_char( uint8_t *screen_addr, uint8_t c );
+
+/* main() has to come first, with no CRT the first function goes in at 0x0000 */
 void main(void)
 {
   zx_cls( PAPER_WHITE|INK_BLACK );
   zx_border(0);
+
+  print_str( 0, 12, "Hello world" );
 
   uint8_t border_colour = INK_BLACK;
   while(1)
@@ -26,3 +32,27 @@ void main(void)
   }
 
 }
+
+void print_str( uint8_t x, uint8_t y, uint8_t *str )
+{
+  uint8_t i;
+
+  for( i=0; i<strlen(str); i++ )
+  {
+    uint8_t *addr = (uint8_t*)(0x4800+i); //zx_cxy2saddr(x+i, y);
+    print_char( addr, *(str+i) );
+  }
+}
+
+void print_char( uint8_t *screen_addr, uint8_t c )
+{
+  extern uint8_t font[];
+
+  uint8_t i;
+  for(i=0;i<8;i++)
+  {
+    *screen_addr = font[((c-' ')*8)+i];
+    screen_addr += 256;
+  }
+}
+
